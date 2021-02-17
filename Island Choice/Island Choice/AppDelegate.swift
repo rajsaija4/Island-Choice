c08c55680d0049dc6e9a5dd0f93ba7c310aa1bd7
Ashish <ashish.gurutechnolabs@gmail.com>
2021-02-17 19:16:24 +0530

    initial commit

---
+++ Island Choice/Island Choice/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Island Choice
+//
+//  Created by GT-Ashish on 17/02/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
